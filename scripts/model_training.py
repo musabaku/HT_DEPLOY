1d61df1ec6824c6af68c8c8050bd30794e426ad6
Musab <musabkhan2003@gmail.com>
2024-05-28 11:11:49 +0300

    Initial commit

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -12,7 +12,6 @@ y_train = np.load('/data/y_train.npy')
 # Random Forest
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_model.fit(X_train, y_train)
-# joblib.dump(rf_model, '../models/rf_model.pkl')
 model_path = 'C:/Users/musab/Desktop/project/models/rf_model.pkl'
 
 # Save the model
@@ -22,7 +21,6 @@ lgb_model = lgb.LGBMClassifier()
 lgb_model.fit(X_train, y_train)
 model_path = 'C:/Users/musab/Desktop/project/models/lgbm_model.pkl'
 
-# joblib.dump(lgb_model, '../models/lgbm_model.pkl')
 joblib.dump(lgb_model, model_path)
 
 # XGBoost
@@ -36,26 +34,4 @@ xgb_model = xgb.XGBClassifier(
 xgb_model.fit(X_train, y_train_encoded)
 model_path = 'C:/Users/musab/Desktop/project/models/xgb_model.pkl'
 
-# joblib.dump(xgb_model, '../models/xgb_model.pkl')
 joblib.dump(xgb_model, model_path)
-
-# import numpy as np
-# from sklearn.ensemble import RandomForestClassifier
-# import joblib
-
-# # Load preprocessed data
-# X_train = np.load('/data/X_train.npy')
-# X_test = np.load('/data/X_test.npy')
-# y_train = np.load('/data/y_train.npy')
-# y_test = np.load('/data/y_test.npy')
-
-# # Train model
-# random_forest = RandomForestClassifier(n_estimators=100, random_state=42)
-# random_forest.fit(X_train, y_train)
-
-# # Save model
-# # joblib.dump(random_forest, '/models/model.pkl')
-# model_path = 'C:/Users/musab/Desktop/project/models/model.pkl'
-
-# # Save the model
-# joblib.dump(random_forest, model_path)
